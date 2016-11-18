2bbd6f71d91124c8e4072b0e5eba1338984b4a02
buele <info@raffaelebua.eu>
2016-11-17 19:03:36 -0500

    stabilizing plugin template

--- scientific-processor/plugins/template/Sample.cc
+++ scientific-processor/plugins/template/Sample.cc
@@ -30,61 +30,20 @@ std::string Sample::concatString(const char * s1, const char * s2){
 }
 
 void Sample::processRasterData(float * rasterMatrix, int nXSize, int nYSize, float * dest){
-
   for(int i=0; i< nXSize*nYSize; i++){
     dest[i]=(float)rasterMatrix[i] / (float)10000;
-    std::cout << rasterMatrix[i] << "->" << dest[i] << std::endl;
   }
-
-
 }
-
-void Sample::manageRasterIO(GDALRasterBand * band, int nXSize, int nYSize, GDALRWFlag flag, float * rasterMatrix){
-  CPLErr err = band->RasterIO( flag, 	    //GDALRWFlag  	eRWFlag
-			      0,       	    // int  	    nXOff
-			      0,       	    // int  	    nYOff
-			      nXSize,  	    //int  	        nXSize
-			      nYSize,       //int  	        nYSize
-                  rasterMatrix, //void      *  	pData
-			      nXSize, 	    //int  	        nBufXSize
-			      nYSize, 	    //int  	        nBufYSize
-			      GDT_Float32,  //GDALDataType  eBufType
-                  0, 		    //GSpacing  	nPixelSpace
-			      0);           //GSpacing  	nLineSpace
-  std::cout << err << std::endl;
-}
-
-void rasterIO(GDALDataset * dataset, int nXSize, int nYSize, GDALRWFlag flag, float * rasterMatrix){
-
-
- dataset->RasterIO 	( 	flag,
-		0, //int  	nXOff,
-		0, //int  	nYOff,
-		nXSize, ///int  	nXSize,
-		nYSize, //int  	nYSize,
-		rasterMatrix,//void *  	pData,
-		nXSize, //int  	nBufXSize,
-		nYSize, //int  	nBufYSize,
-		GDT_Float32, //GDALDataType  	eBufType,
-		1, //int  	nBandCount,
-		NULL, //int *  	panBandMap,
-		0, //GSpacing  	nPixelSpace,
-		0, //GSpacing  	nLineSpace,
-		0, //GSpacing  	nBandSpace,
-		NULL //GDALRasterIOExtraArg *  	psExtraArg
-	);
-	}
 
 
 void Sample::process(char * productPath, char * destinationPath){
 
+
+
   const char * DEST_FORMAT = "GTiff";
-  const char * PROCESSED_IMAGE_FILENAME = "processedsmall.tif";
-  const char * TMP_IMAGE_FILENAME = "small.tif";
+  const char * PROCESSED_IMAGE_FILENAME = "pB06.tif";
+  const char * TMP_IMAGE_FILENAME = "B06.tif";
   GDALDriver * poDriver;
-
-  //char **  	srcMetadata;
-
 
 
   // load drivers
@@ -93,26 +52,18 @@ void Sample::process(char * productPath, char * destinationPath){
   // load driver for geotiff
   poDriver = GetGDALDriverManager()->GetDriverByName(DEST_FORMAT);
 
-
+  // calculate source and destination paths
   std::string absolutePath = this->concatString(productPath, TMP_IMAGE_FILENAME);
   std::string destinationAbsolutePath = this->concatString(destinationPath, PROCESSED_IMAGE_FILENAME);
+  std::cout << "tmp source" << absolutePath << std::endl;
+  std::cout << "destination path " << destinationAbsolutePath << std::endl;
+
+
 
   // open origin file
   GDALDataset *poSrcDS = (GDALDataset *) GDALOpen( absolutePath.c_str(), GA_Update );
-  std::cout << "tmp source" << absolutePath << std::endl;
-  std::cout << "destination path " << destinationAbsolutePath << std::endl;
+
   GDALDataset *poDstDS;
-  //srcMetadata = poSrcDS->GetMetadata();
-
-
-  // ---- //
-//  char **papszOptions = NULL;
-//  papszOptions = CSLSetNameValue( papszOptions, "ot", "Float32" );
-
-  // ---- //
-
-  // create a copy
-  //poDstDS = poDriver->CreateCopy( destinationAbsolutePath.c_str(), poSrcDS, FALSE,papszOptions, NULL, NULL  );
 
   // get band
   GDALRasterBand  *poBand;
@@ -124,55 +75,36 @@ void Sample::process(char * productPath, char * destinationPath){
   int   nXSize = poBand->GetXSize();
   int   nYSize = poBand->GetYSize();
 
-
-   // new file
+  // create new file
   poDstDS = poDriver->Create( destinationAbsolutePath.c_str(), nXSize, nYSize, 1, GDT_Float32,NULL );
-  //------//
-  //double adfGeoTransform[6] = { 444720, 30, 0, 3751320, 0, -30 };
+
+  // copy geo tranformation
   double adfGeoTransform[6];
-  //OGRSpatialReference oSRS;
-  //char *pszSRS_WKT = NULL;
-  //GDALRasterBand *poBand;
   poSrcDS->GetGeoTransform(adfGeoTransform);
   poDstDS->SetGeoTransform( adfGeoTransform );
-  //oSRS.SetUTM( 11, TRUE );
-  //oSRS.SetWellKnownGeogCS( "NAD27" );
-  //oSRS.exportToWkt( &pszSRS_WKT );
 
-  const char *  	pszProjection;
-
-
-
-  //SetProjection 	( 	const char *  	pszProjection	);
+  // copy projection
+  const char * pszProjection;
   pszProjection = poSrcDS->GetProjectionRef();
   poDstDS->SetProjection(pszProjection);
-  //  CPLFree( pszSRS_WKT );
-  //poBand = poDstDS->GetRasterBand(1);
-  //poBand->RasterIO( GF_Write, 0, 0, 512, 512,abyRaster, 512, 512, GDT_Byte, 0, 0 );
-  /* Once we're done, close properly the dataset */
-  //GDALClose( (GDALDatasetH) poDstDS );
 
 
-  //-----//
-
-  //poDstDS->SetMetadata(srcMetadata,NULL);
-
-  std::cout << "nXSize: " << nXSize << ", nYSize: " << nYSize << std::endl;
+  // allocate memory
   rasterMatrix = (float *) CPLMalloc(sizeof(float)*nXSize * sizeof(float)*nYSize);
   dest = (float *) CPLMalloc(sizeof(float)*nXSize * sizeof(float)*nYSize);
 
-  //rasterIO(poSrcDS, nXSize, nYSize, GF_Read, rasterMatrix);
-
   // read raster data
-  this->manageRasterIO(poBand, nXSize, nYSize,  GF_Read, rasterMatrix);
+  //this->manageRasterIO(poBand, nXSize, nYSize,  GF_Read, rasterMatrix);
+  CPLErr err = poBand->RasterIO(GF_Read,0,0,nXSize,nYSize,rasterMatrix,nXSize,nYSize,GDT_Float32,0,0);
+  std::cout << err << std::endl;
 
   // process raster data
   this->processRasterData(rasterMatrix,nXSize,nYSize,dest);
 
-  rasterIO(poDstDS, nXSize, nYSize, GF_Write, dest);
-
-  // write raster data
-  //this->manageRasterIO(poBand, nXSize, nYSize,  GF_Write, dest);
+  // write data
+  //this->manageRasterIO(poDstDS, nXSize, nYSize, GF_Write, dest);
+  poDstDS->RasterIO(GF_Write,0,0,nXSize,nYSize,rasterMatrix,nXSize,nYSize,GDT_Float32,1,NULL,0,0,0,NULL);
+  //rasterIO(poDstDS, nXSize, nYSize, GF_Write, dest);
 
   // close file
   if( poDstDS != NULL )
@@ -180,6 +112,7 @@ void Sample::process(char * productPath, char * destinationPath){
   if( poSrcDS != NULL )
     GDALClose( (GDALDatasetH) poSrcDS );
 
+  // free allocated memory
   CPLFree( rasterMatrix );
   CPLFree( dest );
 
--- scientific-processor/plugins/template/Sample.h
+++ scientific-processor/plugins/template/Sample.h
@@ -10,7 +10,7 @@ class Sample
 private:
   std::string concatString(const char *  s1, const char * s2);
   void processRasterData(float * rasterData, int nXSize, int nYSize, float * dest);
-  void manageRasterIO(GDALRasterBand * band, int nXSize, int nYSize, GDALRWFlag flag, float * rasterMatrix);
+  //void manageRasterIO(GDALRasterBand * band, int nXSize, int nYSize, GDALRWFlag flag, float * rasterMatrix);
 public:
   Sample();
   const char * PROCESSED_IMAGE_FILENAME ;
