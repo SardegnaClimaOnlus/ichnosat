bb0bc43116b83efe04b4e05a8b19509b1c84137d
Raffaele Bua <info@raffaelebua.eu>
2016-12-31 06:53:39 +0100

    first proof of concepts of scientific processor

--- scientific_processor/src/start.py
+++ scientific_processor/src/start.py
@@ -11,7 +11,10 @@ import os, sys, select
 from flask import Flask, request
 app = Flask(__name__)
 from flask_cors import CORS, cross_origin
+from scientific_processor.src.logger import *
+from scientific_processor.src.ScientificProcessor import ScientificProcessor
 CORS(app)
+
 
 
 def process(source):
@@ -50,7 +53,7 @@ def process(source):
                 logging.debug("source")
                 logging.debug(source)
                 plugin_path = os.path.join(dName, fileName)
-                regex_plugin_name = re.escape(dName )+ '\/(.*?)\.so'
+                regex_plugin_name = re.escape(dName ) + '\/(.*?)\.so'
                 plugin_name = re.match(regex_plugin_name, plugin_path).group(1)
 
                 destination = outbox_path + product_name + "-" + plugin_name + '/'
@@ -78,66 +81,35 @@ def process(source):
 
 
 
+
 @app.route('/process', methods=['POST'])
 def process_req():
     logging.debug("><><><><><><><><><>--->  /process ")
     logging.debug("||||||||||||||| OOOOOO OOOO0000OOO0O0O0O0||||| ><><><><><><><><><>--->  request.get_json() ::::")
-
-
     obj = request.get_json()
-
     logging.debug("obj['path']--------------------------------------")
     logging.debug(obj['path'])
-
-
+    #logging.debug("queue:")
+    #logging.debug("app.pippo|||||||||||||||||||||||||||||||||||||||||XXX: " )
+    #app.queue.append(obj['path'])
+    #logging.debug(app.queue)
+    app.scientific_processor.push_product(obj['path'])
     #process(request.get_json()["path"])
     return obj['path'] #obj["this"]
 
 
+
 def main():
     logging.debug("><><><><><><><><><>--->  scientific-processor")
+    #app.queue = []
+    logging.debug("main()")
+    logging.debug(">>>> create a new scientific_processor")
+    app.scientific_processor = ScientificProcessor()
+    logging.debug(">>>> created new scientific_processor")
+    logging.debug(app.scientific_processor)
     app.run(debug=True, host='0.0.0.0', port=5002)
+
     return
-   #  logging.debug("><><><><$%&%$&%$%$&&%$ START SCIENTIFIC PROCESSOR!!!")
-   # # scientificProcessor = ScientificProcessor()
-   #  print("############# hello from scientific processor start ########")
-   #
-   #  connection = pika.BlockingConnection(pika.ConnectionParameters(
-   #      host='localhost', heartbeat_interval=800))
-   #  channel = connection.channel()
-   #
-   #  channel.queue_declare(queue='hello')
-   #
-   #  def callback(ch, method, properties, body):
-   #      data = json.loads(body.decode("utf-8") )
-   #
-   #      logging.debug('>>>>>>>>>>>>start processing of new product')
-   #      print('>>>>>>>>>>>>start processing of new product')
-   #
-   #      if(os.path.isdir(data["source"])):
-   #          process(data["source"])
-   #          logging.debug('COMPLETED processing for the product with path: ' + data['source'])
-   #      else:
-   #          logging.debug("product not found - " + data["source"])
-   #      logging.debug('completed processing of new product')
-   #
-   #  channel.basic_consume(callback,
-   #                        queue='hello',
-   #                        no_ack=True)
-   #
-   #
-   #  print(' scientific_processor::: To exit press CTRL+C')
-   #  #channel.start_consuming()
-   #  logging.debug('STARTED SCIENTIFIC-PROCESSOR!!!!')
-   #
-   #
-   #  try:
-   #      channel.start_consuming()
-   #  except KeyboardInterrupt:
-   #      channel.stop_consuming()
-   #  connection.close()
-
-
 
 
 def start_scientific_processor():
