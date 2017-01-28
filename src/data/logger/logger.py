90651befcd1eaf7b6b0b62b506436c4c0e9b2b3e
Raffaele Bua <info@raffaelebua.eu>
2017-01-28 18:25:52 +0100

    change logger level

--- src/data/logger/logger.py
+++ src/data/logger/logger.py
@@ -3,7 +3,7 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read("/usr/ichnosat/src/data/logger/config/logger.cfg")
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     filename=config['DEFAULT']['LogFolder'] + config['DEFAULT']['LogFilename'],
                     format='%(asctime)s %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p')
