dd1d2712a62033d5bb2367965372e5923d5cea9a
Raffaele Bua <info@raffaelebua.eu>
2017-01-28 18:25:25 +0100

    implement configurable cron

--- src/core/system_manager/init.py
+++ src/core/system_manager/init.py
@@ -1,6 +1,7 @@
 import sys
 from crontab import CronTab
 sys.path.append('/usr/ichnosat/')
+import configparser
 from src.core.system_manager.system_manager import SystemManager
 from src.data.logger.logger import logger
 
@@ -13,9 +14,12 @@ sm.compile_plugins()
 
 
 # set cron tab
-cron = CronTab(user='root')
-job = cron.new(command='wget -qO- http://localhost:5000/start-downloader &> /dev/null')
-job.setall('10 10 * * *')
+config = configparser.ConfigParser()
+config_file_path = "/usr/ichnosat/src/core/system_manager/config/config.cfg"
+
+cron = CronTab(user=config['CRON']['user'])
+job = cron.new(command=config['CRON']['command'])
+job.setall(config['CRON']['cron'])
 cron.write()
 
 
