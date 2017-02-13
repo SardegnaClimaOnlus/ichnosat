6208fad859a54069e505fcc730ac26f90e23c0e9
Raffaele Bua <info@raffaelebua.eu>
2017-02-13 01:30:57 +0100

    auto-docs AmazonBucketManager

--- src/core/downloader/AmazonBucketManager.py
+++ src/core/downloader/AmazonBucketManager.py
@@ -49,18 +49,66 @@ __contact__ = "info@raffaelebua.eu"
 __status__ = "Development"
 
 class AmazonBucketManager:
+    """ This class is an Adapter of Amazon AWS services.
+        Main goal of this class is **Retrieve the list of products from aws via tile list, and time interval**
+    """
     def __init__(self, configurations):
+
         self.config = configurations
         self.product_list = []
         self.last_item = None
         return
 
     def generate_url(self, tile, year):
+        """ This method generates the aws url from the tile name string and the start year of filter interval.
+            AWS service has the *prefix* parameter, to filter the list of available files in the bucket.
+            This method generates the proper *prefix* parameter value, to give the list of files
+            available in the year of *year* argument passed in the method.
+
+            The generated parameter is like:
+
+            .. code-block:: bash
+
+                prefix=tiles/{tile}/{year}/
+
+            :param tile: The tile name string with a pattern like
+
+            .. code-block:: bash
+
+                32/T/MK
+
+            :param year: The year
+            :returns: request url
+            :rtype: String
+        """
         url_template = 'http://sentinel-s2-l1c.s3.amazonaws.com/?list-type=2&prefix=tiles/{tile}/{year}/'
         url = url_template.format(tile=tile, year=year)
         return url
 
     def extract_date(self, item):
+        """ This method extracts the date from the tile name string (complete with the date).
+
+            :param item: The tile name complete string. e.g.
+
+            .. code-block:: bash
+
+                tiles/32/T/MK/YYYY/MM/DD/
+
+            where
+                * **YYYY** is the year
+                * **MM** is the month
+                * **DD** is the day
+
+            :returns: The extracted date form the tile name string
+            :rtype: Datetime
+            The method apply the regex:
+
+            .. code-block:: bash
+
+                tiles/[0-9]2/[A-Z]/[A-Z]{2}/([0-9]{4})/([0-9]*)/([0-9]*)/
+
+            to the *item* parameter
+        """
         regex = 'tiles/[0-9]2/[A-Z]/[A-Z]{2}/([0-9]{4})/([0-9]*)/([0-9]*)/'
         match = re.search(regex, item)
         year = match.group(1)
@@ -69,6 +117,22 @@ class AmazonBucketManager:
         return datetime.date(int(year), int(month), int(day))
 
     def load_products(self, tile, year, paginated=False):
+        """ *load_products* load list products form the Amazon *AWS* Bucket of Sinergise *Sentinel-2 on AWS*.
+            This method contains the *AWS* APIs semantic it is the core of Adapter.
+            It manages also pagination.
+
+            :param tile: Tile name
+            :type tile: String
+            :param year: Year of interest, the method extract the whole list of products for this year
+            :type year: String
+            :param paginated: if this parameter is *True* that means that the list provided from AWS is
+                              paginated and the next page starts after the *last_item*
+            :type year: Boolean
+            :returns: None
+            :rtype: None
+
+            This method stores the list of products in the classes property *product_list*.
+        """
         logger.debug("(AmazonBucketManager load_products)")
         # Generate url for year
         url = self.generate_url(tile, year)
@@ -96,6 +160,18 @@ class AmazonBucketManager:
             return False
 
     def get_products_list(self, searchFilter):
+        """ This method manages the retrieval of aws products.
+            This method contains the *AWS* APIs semantic it is the core of Adapter.
+            It manages also pagination.
+
+            :param searchFilter: Search parameters to filter the available files in the bucket
+            :type searchFilter: SearchFilter
+
+            :returns: The pending products available in the Amazon bucket.
+            :rtype: None
+
+            .. todo:: Optimize list retrieval using set end date year instead *NOW* year.
+        """
         logger.debug("(AmazonBucketManager get_products_list)")
         tile = searchFilter.tile
         self.product_list = []
@@ -104,6 +180,7 @@ class AmazonBucketManager:
         current_year = datetime.datetime.now().year
         pending_products = []
         # Extract whole list of products via amazon, from start year to now
+        # TODO: OPTMIZE IT USING THE END DATE FROM CONFIGURATION AND NOT *NOW*
         while year <= current_year:
             is_truncated = self.load_products(tile, year, False)
             while is_truncated:
